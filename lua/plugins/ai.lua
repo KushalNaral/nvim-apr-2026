fc901c267fef6d5f9895f11af45806ee50628669
Kushal <kushal.naral@yenyasoft.com>
2026-04-08 12:32:26 +0545

    UPDATE : Added new neocodium ai assitant ( inline ) as ai.lua in the plugins

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,19 @@
+-- AI / inline completions (Codeium via neocodeium)
+return {
+  {
+    'monkoose/neocodeium',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local neocodeium = require 'neocodeium'
+      neocodeium.setup()
+      vim.keymap.set('i', '<A-l>', neocodeium.accept, { desc = 'NeoCodeium accept' })
+      vim.keymap.set('i', '<A-w>', neocodeium.accept_word, { desc = 'NeoCodeium accept word' })
+      vim.keymap.set('i', '<A-a>', neocodeium.accept_line, { desc = 'NeoCodeium accept line' })
+      vim.keymap.set('i', '<A-]>', function() neocodeium.cycle_or_complete(1) end, { desc = 'NeoCodeium next' })
+      vim.keymap.set('i', '<A-[>', function() neocodeium.cycle_or_complete(-1) end, { desc = 'NeoCodeium prev' })
+      vim.keymap.set('i', '<A-c>', neocodeium.clear, { desc = 'NeoCodeium clear' })
+      vim.keymap.set('i', '<A-e>', neocodeium.toggle, { desc = 'NeoCodeium toggle' })
+    end,
+  },
+}
